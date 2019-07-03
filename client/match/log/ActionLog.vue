2e14f04655dc7a8aa047ccd30b46da62e3084b1d
augustalex <augustalexandersson@gmail.com>
2019-07-03 15:09:49 +0200

    Fixed "S" being html-encoded in the action log

--- client/match/log/ActionLog.vue
+++ client/match/log/ActionLog.vue
@@ -53,10 +53,12 @@
             getEntryHtml(entry) {
                 const textWithSubstitutedSensitiveCharacters = entry.text
                     .split('&').join('_amp_')
-                    .split('s').join('_115_');
+                    .split('s').join('_115_')
+                    .split('S').join('_83_');
                 return encodeHtml(textWithSubstitutedSensitiveCharacters)
                     .split('_amp_').join('&')
                     .split('_115_').join('s')
+                    .split('_83_').join('S')
                     .split(/\*/).join('<strong>')
                     .split(/#/).join('</strong>');
             },
