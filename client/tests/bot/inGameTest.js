652650aeeb48605fcd9c15a8729d5bd88ebb58de
August Alexandersson <augustalexandersson@gmail.com>
2019-09-07 19:19:57 +0200

    Bot: Refactor

--- client/tests/bot/inGameTest.js
+++ client/tests/bot/inGameTest.js
@@ -8,52 +8,50 @@ const {
     refute,
 } = require('../../testUtils/bocha-jest/bocha-jest.js');
 
-describe('In Game', () => {
-    describe('In Draw phase', () => {
-        test('When can draw 1 more card should draw a card', async () => {
-            const { matchController } = await setupFromState({
-                phase: 'draw',
-                stationCards: [
-                    unflippedStationCard('S1A', 'draw')
-                ],
-                cardsOnHand: [
-                    createCard({ id: 'C2A' })
-                ]
-            });
-
-            assert.calledWith(matchController.emit, 'drawCard');
+describe('In Draw phase', () => {
+    test('When can draw 1 more card should draw a card', async () => {
+        const { matchController } = await setupFromState({
+            phase: 'draw',
+            stationCards: [
+                unflippedStationCard('S1A', 'draw')
+            ],
+            cardsOnHand: [
+                createCard({ id: 'C2A' })
+            ]
         });
 
-        test('When is not draw phase should NOT draw card', async () => {
-            const { matchController } = await setupFromState({
-                phase: 'action',
-                stationCards: [
-                    unflippedStationCard('S1A', 'draw')
-                ],
-                cardsOnHand: [
-                    createCard({ id: 'C2A' })
-                ]
-            });
+        assert.calledWith(matchController.emit, 'drawCard');
+    });
 
-            refute.calledWith(matchController.emit, 'drawCard');
+    test('When is not draw phase should NOT draw card', async () => {
+        const { matchController } = await setupFromState({
+            phase: 'action',
+            stationCards: [
+                unflippedStationCard('S1A', 'draw')
+            ],
+            cardsOnHand: [
+                createCard({ id: 'C2A' })
+            ]
         });
 
-        test('When cannot draw card should NOT draw card', async () => {
-            const { matchController } = await setupFromState({
-                turn: 1,
-                phase: 'draw',
-                stationCards: [
-                    unflippedStationCard('S1A', 'draw')
-                ],
-                cardsOnHand: [
-                    createCard({ id: 'C2A' })
-                ],
-                events: [
-                    DrawCardEvent({ turn: 1 })
-                ]
-            });
+        refute.calledWith(matchController.emit, 'drawCard');
+    });
 
-            refute.calledWith(matchController.emit, 'drawCard');
+    test('When cannot draw card should NOT draw card', async () => {
+        const { matchController } = await setupFromState({
+            turn: 1,
+            phase: 'draw',
+            stationCards: [
+                unflippedStationCard('S1A', 'draw')
+            ],
+            cardsOnHand: [
+                createCard({ id: 'C2A' })
+            ],
+            events: [
+                DrawCardEvent({ turn: 1 })
+            ]
         });
+
+        refute.calledWith(matchController.emit, 'drawCard');
     });
 });
