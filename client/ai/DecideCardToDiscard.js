4b274b9a15c363ab2b5e693f3dd0119aa5d50069
August Alexandersson <augustalexandersson@gmail.com>
2019-09-09 21:03:26 +0200

    Bot: Discard phase

--- client/ai/DecideCardToDiscard.js
+++ client/ai/DecideCardToDiscard.js
@@ -1,18 +1,18 @@
 module.exports = function DecideCardToDiscard({ playerStateService }) {
     return () => {
         const cards = playerStateService.getCardsOnHand();
-        const eventCardToDiscard = cards
-            .filter(c => c.type === 'event')
-            .slice()
-            .sort((a, b) => a.cost - b.cost)
-            [0];
+        const eventCardToDiscard = chooseCheapestCardOfType(cards, 'event');
         if (eventCardToDiscard) return eventCardToDiscard.id;
 
-        const spaceShipToDiscard = cards
-            .filter(c => c.type === 'spaceShip')
-            .slice()
-            .sort((a, b) => a.cost - b.cost)
-            [0];
+        const spaceShipToDiscard = chooseCheapestCardOfType(cards, 'spaceShip');
         return spaceShipToDiscard.id;
     };
 };
+
+function chooseCheapestCardOfType(cards, type) {
+    return cards
+        .filter(c => c.type === type)
+        .slice()
+        .sort((a, b) => a.cost - b.cost)
+        [0];
+}
