da976a0d0632457965a5eb85fbcfabfec4eea32a
August Alexandersson <augustalexandersson@gmail.com>
2019-09-13 21:58:04 +0200

    Bot: Refactor

--- client/tests/bot/SelectingStartingStationCards_integrationTest.js
+++ client/tests/bot/SelectingStartingStationCards_integrationTest.js
@@ -8,37 +8,6 @@ const Commander = require('../../../shared/match/commander/Commander.js');
 const { setupFromState, BotId, PlayerId } = require('./botTestHelpers.js');
 const { unflippedStationCard } = require('../../testUtils/factories.js');
 
-test('When does NOT have control of turn should NOT emit anything', async () => {
-    const { matchController } = await setupFromState({
-        mode: MatchMode.game,
-        currentPlayer: PlayerId,
-        phase: 'action',
-        cardsOnHand: [{ id: 'C1A', cost: 0 }],
-    });
-
-    expect(matchController.emit).not.toBeCalled();
-});
-
-describe('Selecting starting player', () => {
-    test('When is choosing starting player should select starting player', async () => {
-        const { matchController } = await setupFromState({
-            mode: 'chooseStartingPlayer',
-            currentPlayer: BotId
-        });
-
-        expect(matchController.emit).toBeCalledWith('selectPlayerToStart', { playerToStartId: BotId });
-    });
-
-    test('When opponent is choosing starting player should NOT select starting player', async () => {
-        const { matchController } = await setupFromState({
-            mode: 'selectStartingPlayer',
-            currentPlayer: PlayerId
-        });
-
-        expect(matchController.emit).not.toBeCalledWith('selectPlayerToStart');
-    });
-});
-
 describe('In match mode for "select starting station cards"', () => {
     test('should put down first starting station card in the draw-row', async () => {
         const { matchController } = await setupFromState({
@@ -174,6 +143,4 @@ describe('In match mode for "select starting station cards"', () => {
 
         expect(matchController.emit).not.toBeCalledWith('selectStartingStationCard');
     });
-
-    test.todo('SHOULD BE ABLE TO MANAGE WHEN OPPONENT STARTS THE GAME VS BOT STARTS THE GAME');
 });
