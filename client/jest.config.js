ae6496bed1693fc633b8315d8ec84f1ae24be383
August Alexandersson <augustalexandersson@gmail.com>
2019-09-19 22:35:17 +0200

    Jest now detects all test files ending with .test.js, no mather what folder they are in

--- client/jest.config.js
+++ client/jest.config.js
@@ -22,8 +22,14 @@ module.exports = {
     testEnvironment: 'jsdom',
 
     // The glob patterns Jest uses to detect test files
-    // testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
-    testMatch: ['**/tests/?(*)+(Test).js?(x)', '**/tests/legacy/?(*)+(Test).js?(x)', '**/tests/bot/?(*)+(Test).js?(x)'],
+    testMatch: ['**/?(*.)+(spec|test).js?(x)'],
+    // testMatch: [
+    //     'Test.js',
+    //     '(*)+(Test).js',
+    //     // '**/tests/legacy/?(*)+(Test).js?(x)',
+    //     // '**/tests/bot/?(*)+(Test).js?(x)',
+    //     // '**/tests/*?(*)+(Test).js?(x)'
+    // ],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     testPathIgnorePatterns: ['\\\\node_modules\\\\'],
