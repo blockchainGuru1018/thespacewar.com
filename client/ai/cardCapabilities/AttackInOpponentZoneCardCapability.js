034eb0828132d7a70225a2bb1979a94a31685045
August Alexandersson <augustalexandersson@gmail.com>
2019-09-21 16:44:59 +0200

    Bot: Attack phase - can attack cards in opponent zone

--- client/ai/cardCapabilities/AttackInOpponentZoneCardCapability.js
+++ client/ai/cardCapabilities/AttackInOpponentZoneCardCapability.js
@@ -1,11 +1,30 @@
 module.exports = function ({
-    card
+    card,
+    opponentStateService,
+    matchController
 }) {
     return {
-        canDoIt
+        canDoIt,
+        doIt
     };
 
     function canDoIt() {
-        return !card.inHomeZone();
+        return !card.inHomeZone()
+            && targets().length > 0;
+    }
+
+    function doIt() {
+        matchController.emit('attack', {
+            attackerCardId: card.id,
+            defenderCardId: firstTarget().id
+        });
+    }
+
+    function firstTarget() {
+        return targets()[0];
+    }
+
+    function targets() {
+        return opponentStateService.getMatchingBehaviourCards(opponentCard => card.canAttackCard(opponentCard));
     }
 };
--- client/tests/bot/cardCapabilities/AttackInOpponentZoneCardCapability.test.js
+++ client/tests/bot/cardCapabilities/AttackInOpponentZoneCardCapability.test.js
@@ -3,18 +3,63 @@
  */
 const Capability = require('../../../ai/cardCapabilities/AttackInOpponentZoneCardCapability.js');
 
-test('Player can attack if card in opponent zone and there is an available target', () => {
-    const capability = Capability({
-        card: { inHomeZone: () => false }
+describe('canDoIt', () => {
+    test('Player can attack if card in opponent zone and there is an available target', () => {
+        const target = { id: 'C1A' };
+        const capability = Capability({
+            card: { inHomeZone: () => false, canAttackCard: _target => _target.id === 'C1A' },
+            opponentStateService: {
+                getMatchingBehaviourCards: matcher => matcher(target) ? [target] : null
+            }
+        });
+
+        expect(capability.canDoIt()).toBe(true);
     });
 
-    expect(capability.canDoIt()).toBe(true);
-});
+    test('Player can NOT attack if card is NOT in opponent zone', () => {
+        const capability = createCapability({
+            card: { inHomeZone: () => true }
+        });
 
-test('Player can NOT attack if card is NOT in opponent zone', () => {
-    const capability = Capability({
-        card: { inHomeZone: () => true }
+        expect(capability.canDoIt()).toBe(false);
     });
 
-    expect(capability.canDoIt()).toBe(false);
+    test('Player can NOT attack if there are no available targets', () => {
+        const capability = createCapability({
+            card: { inHomeZone: () => false },
+            opponentStateService: {
+                getMatchingBehaviourCards: () => []
+            }
+        });
+
+        expect(capability.canDoIt()).toBe(false);
+    });
 });
+
+describe('doIt', () => {
+    test('Should emit attack for first matching target', () => {
+        const matchController = { emit: jest.fn() };
+        const targets = [{ id: 'C2A' }, { id: 'C3A' }];
+        const capability = createCapability({
+            card: { id: 'C1A' },
+            opponentStateService: {
+                getMatchingBehaviourCards: () => targets
+            },
+            matchController
+        });
+
+        capability.doIt();
+
+        expect(matchController.emit).toBeCalledWith('attack', { attackerCardId: 'C1A', defenderCardId: 'C2A' });
+    });
+});
+
+function createCapability(options = {}) {
+    return Capability({
+        card: { inHomeZone: () => false },
+        opponentStateService: {
+            getMatchingBehaviourCards: () => []
+        },
+        ...options
+    });
+}
