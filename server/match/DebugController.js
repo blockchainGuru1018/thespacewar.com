8e78a5ec2b4dbee053cfa26cb451f687c5d60604
August Alexandersson <augustalexandersson@gmail.com>
2019-10-09 17:27:06 +0200

    Reintroducing save/restore game functionality for debugging purposes only

--- server/match/DebugController.js
+++ server/match/DebugController.js
@@ -1,18 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 const SAVE_FOLDER = path.join(__dirname, 'save');
-const Deck = require('../deck/Deck.js');
-const CardDataAssembler = require('../../shared/CardDataAssembler.js');
 
 function DebugController(deps) {
 
     const {
         matchService,
-        restoreFromState,
-        rawCardDataRepository
+        restoreFromState
     } = deps;
-
-    let cardDataAssembler = CardDataAssembler({ rawCardDataRepository });
 
     return {
         onSaveMatch,
@@ -20,47 +15,46 @@ function DebugController(deps) {
         timeAlive
     };
 
-    function onSaveMatch(playerId, saveName) {
-        throw new Error('Not supported at the moment');
-
-        // let filePath = path.join(SAVE_FOLDER, `${saveName}.json`);
-        //
-        // let stateCopy = { ...matchService.getState() };
-        // const [firstPlayerId, secondPlayerId] = stateCopy.playerOrder;
-        //
-        // let stateJson = JSON.stringify({
-        //     state: stateCopy,
-        //     saverPlayerId: playerId
-        // });
-        // fs.writeFileSync(filePath, stateJson, 'utf8');
+    function onSaveMatch(saverPlayerId, saveName) {
+        const state = { ...matchService.getState() };
+        const stateJson = JSON.stringify({ state, saverPlayerId });
+        storeSavedGameJson(filePath(saveName), stateJson);
     }
 
     function onRestoreSavedMatch(playerId, { saveName, opponentId }) {
-        throw new Error('Not supported at the moment');
+        const { state: restoredState, saverPlayerId } = readSavedGameData(saveName);
+        const previousOpponentId = restoredState.playerOrder.find(id => id !== saverPlayerId);
 
-        // let filePath = path.join(SAVE_FOLDER, `${saveName}.json`);
-        // let restoredStateJson = fs.readFileSync(filePath, 'utf8');
-        //
-        // let { state: restoredState, saverPlayerId } = JSON.parse(restoredStateJson);
-        // let previousOpponentId = restoredState.playerOrder.find(id => id !== saverPlayerId);
-        //
-        // const newPlayerStateById = {
-        //     [playerId]: restoredState.playerStateById[saverPlayerId],
-        //     [opponentId]: restoredState.playerStateById[previousOpponentId]
-        // };
-        // restoredState.playerStateById = newPlayerStateById;
-        //
-        // restoredState.currentPlayer = restoredState.currentPlayer === saverPlayerId ? playerId : opponentId;
-        // let { playerOrder } = matchService.getState();
-        // restoredState.playerOrder = playerOrder;
-        //
-        // restoreFromState(restoredState);
+        const newPlayerStateById = {
+            [playerId]: restoredState.playerStateById[saverPlayerId],
+            [opponentId]: restoredState.playerStateById[previousOpponentId]
+        };
+        restoredState.playerStateById = newPlayerStateById;
+
+        restoredState.currentPlayer = restoredState.currentPlayer === saverPlayerId ? playerId : opponentId;
+        let { playerOrder } = matchService.getState();
+        restoredState.playerOrder = playerOrder;
+
+        restoreFromState(restoredState);
     }
 
     function timeAlive() {
         const startTime = matchService.getGameStartTime();
         if (!startTime) return Infinity;
         return Date.now() - startTime;
+    }
+
+    function readSavedGameData(saveName) {
+        const stateJson = fs.readFileSync(filePath(saveName), 'utf8');
+        return JSON.parse(stateJson);
+    }
+
+    function storeSavedGameJson(saveFilePath, stateJson) {
+        fs.writeFileSync(saveFilePath, stateJson, 'utf8');
+    }
+
+    function filePath(saveName) {
+        return path.join(SAVE_FOLDER, `${saveName}.json`);
     }
 }
 
