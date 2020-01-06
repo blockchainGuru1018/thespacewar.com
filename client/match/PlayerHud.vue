9593ced54a4c84a3c58442f3f5ff5a90b477b28a
August Alexandersson <augustalexandersson@gmail.com>
2020-01-06 16:21:55 +0100

    Refactored large piece out of PlayerHud

--- client/match/PlayerHud.vue
+++ client/match/PlayerHud.vue
@@ -38,171 +38,7 @@
                 </template>
             </div>
 
-            <div class="guideTextContainer" v-if="guideTextContainerVisible">
-
-                <!-- WAITING FOR OPPONENT REQUIREMENT texts -->
-                <div
-                    v-if="waitingForOtherPlayerToFinishRequirements"
-                    class="guideText-waitingForOtherPlayer guideText guideText--small"
-                >
-                    <template v-if="waitingRequirement.reason === 'emptyDeck'">
-                        Your opponent is dealing damage to your station
-                    </template>
-                    <template v-else>
-                        Waiting for other player
-                    </template>
-                </div>
-
-                <!-- ACTION and REQUIREMENT texts -->
-                <div
-                    v-else-if="actionGuideText"
-                    class="guideText guideText--small"
-                >
-                    <div
-                        :style="cardStyle"
-                        class="guideTextCardWrapper card" @click="showEnlargedCard"
-                    >
-                        <div class="enlargeIcon enlargeIcon--small" />
-                    </div>
-                    {{ actionGuideText }}
-                </div>
-                <div
-                    v-else-if="requirementGuideText"
-                    class="guideText guideText--small"
-                >
-                    <div
-                        class="guideTextCardWrapper card"
-                        :style="cardStyle"
-                        @click="showEnlargedCard"
-                    >
-                        <div class="enlargeIcon enlargeIcon--small" />
-                    </div>
-                    {{ requirementGuideText }}
-                    <SkipDrawCard v-if="shouldShowSkipDrawCard" />
-                </div>
-
-                <!-- STARTING GAME texts -->
-                <template v-else-if="selectingStartingStationCards">
-                    <div
-                        v-if="startingStationCardsToPutDownCount > 0"
-                        class="guideText-wrapper"
-                    >
-                        <div class="guideText">
-                            Expand your space station with {{ startingStationCardsToPutDownCount }}
-                            {{ startingStationCardsToPutDownCount === 1 ? 'card' : 'cards' }}
-                        </div>
-                        <div class="guideText-subText">
-                            You lose when all your station cards are damaged
-                        </div>
-                    </div>
-                    <div
-                        v-else-if="playerIsReady"
-                        class="guideText-waitingForOtherPlayer guideText guideText--small"
-                    >
-                        Waiting for other player
-                    </div>
-                </template>
-                <div
-                    v-else-if="waitingForOtherPlayerToSelectStartingPlayer"
-                    class="guideText-waitingForOtherPlayer guideText guideText--small"
-                >
-                    Your opponent is choosing who goes
-                    f
-                    <span style="letter-spacing:.1em;">ir</span>
-                    st
-                </div>
-
-                <!-- SELECT STATION CARDS FOR ATTACK texts -->
-                <div
-                    v-else-if="numberOfStationCardsToSelect > 0"
-                    class="guideText"
-                >
-                    Select {{ numberOfStationCardsToSelect }}
-                    more station {{ numberOfStationCardsToSelect === 1 ? 'card' : 'cards' }}
-                </div>
-
-                <!-- LAST STAND texts -->
-                <div v-else-if="opponentLastStandText">
-                    <div class="guideText">
-                        Your opponent is making a last stand
-                    </div>
-                    <div class="guideText-subText">
-                        {{ opponentLastStandText }}
-                    </div>
-                </div>
-                <div v-else-if="lastStandText">
-                    <div class="guideText">
-                        Last stand
-                    </div>
-                    <div class="guideText-subText">
-                        {{ lastStandText }}
-                    </div>
-                </div>
-
-                <!-- TURN CONTROL texts -->
-                <div
-                    v-else-if="opponentHasControlOfPlayersTurn"
-                    class="guideText-wrapper"
-                >
-                    <div class="guideText">
-                        Your opponent has taken control
-                    </div>
-                    <div class="guideText-subText">
-                        wait to have it back
-                    </div>
-                </div>
-
-                <!-- PHASE texts -->
-                <div
-                    v-else-if="phase === PHASES.draw"
-                    class="guideText-wrapper"
-                >
-                    <div class="guideText-drawPhaseText guideText">
-                        Your turn
-                    </div>
-                    <div class="guideText-drawPhaseSubText guideText-drawCard guideText-subText">
-                        {{ drawCardOrMillText }}
-                    </div>
-                </div>
-                <div
-                    v-else-if="phase === PHASES.preparation"
-                    class="guideText-discardDurationCards guideText guideText--small"
-                >
-                    Discard any duration card you don't want to pay for
-                </div>
-                <div
-                    v-else-if="inDiscardPhaseAndMustDiscardCard"
-                    class="guideText-drawCard guideText guideText--small"
-                >
-                    Discard {{ amountOfCardsToDiscard + (amountOfCardsToDiscard === 1 ? ' card' : ' cards') }} to
-                    continue
-                </div>
-                <template v-else-if="showActionPoints">
-                    <div class="playerActionPoints">
-                        {{ playerActionPointsText }}
-                    </div>
-                </template>
-
-                <!-- WAIT PHASE and TURN CONTROL texts -->
-                <div
-                    v-else-if="turnControl.canToggleControlOfTurn()"
-                    class="guideText-wrapper"
-                >
-                    <div class="guideText">
-                        {{ textOnWaitPhase }}
-                    </div>
-                    <div class="guideText-subText">
-                        <button
-                            class="toggleControlOfTurn darkButton"
-                            title="Or press space to toggle control"
-                            @click="toggleControlOfTurn"
-                            @keydown.space.stop.prevent="toggleControlOfTurn"
-                        >
-                            {{ turnControlButtonText }}
-                        </button>
-                    </div>
-                </div>
-            </div>
+            <GuideText @showEnlargedCard="showEnlargedCard" />
 
             <div
                 v-if="overworkContainerVisible || perfectPlanContainerVisible"
@@ -296,40 +132,33 @@
     import NotificationBannerContainer from "./notificationBanner/NotificationBannerContainer.vue";
     import EndGameHudContainer from "./hud/endGame/EndGameHudContainer.vue";
     import InfoModeContainer from "./infoMode/InfoModeContainer.vue";
+    import GuideText from "./hud/guideText/GuideText.vue";
 
     const Vuex = require('vuex');
     const resolveModuleWithPossibleDefault = require('../../client/utils/resolveModuleWithPossibleDefault.js');
     const FindCard = resolveModuleWithPossibleDefault(require('./findCard/FindCard.vue'));
     const CounterCard = resolveModuleWithPossibleDefault(require('./counterCard/CounterCard.vue'));
     const CounterAttack = resolveModuleWithPossibleDefault(require('./counterAttack/CounterAttack.vue'));
-    const SkipDrawCard = resolveModuleWithPossibleDefault(require('./hud/SkipDrawCard.vue'));
     const { mapState, mapGetters, mapActions } = Vuex.createNamespacedHelpers('match');
     const { mapGetters: mapPermissionGetters } = Vuex.createNamespacedHelpers('permission');
     const cardHelpers = Vuex.createNamespacedHelpers('card');
-    const ghostHelpers = Vuex.createNamespacedHelpers('ghost');
     const requirementHelpers = Vuex.createNamespacedHelpers('requirement');
     const startGameHelpers = Vuex.createNamespacedHelpers('startGame');
-    const infoModeHelpers = Vuex.createNamespacedHelpers('infoMode');
-    const MatchMode = require('../../shared/match/MatchMode.js');
-    const LastStand = require('../../shared/match/LastStand.js');
     const { PHASES } = require('./phases.js');
-    const FatalError = require('../../shared/card/FatalError.js');
 
     export default {
         components: {
+            GuideText,
             InfoModeContainer,
             EndGameHudContainer,
             NotificationBannerContainer,
             FindCard,
             CounterCard,
             CounterAttack,
-            SkipDrawCard
         },
         data() {
             return {
                 enlargedCardVisible: false,
-                lastStandRemainingSeconds: Math.round(LastStand.LastStandLength / 1000),
-                lastStandUpdateIntervalId: null
             };
         },
         computed: {
@@ -337,16 +166,8 @@
                 'readyButtonVisible'
             ]),
             ...mapState([
-                'mode',
-                'currentPlayer',
                 'phase',
-                'ownUser',
-                'opponentUser',
-                'selectedDefendingStationCards',
-                'requirements',
                 'playerCardsOnHand',
-                'readyPlayerIds',
-                'lastStandInfo'
             ]),
             ...mapGetters([
                 'nextPhaseWithAction',
@@ -354,40 +175,27 @@
                 'actionPointsFromStationCards',
                 'maxHandSize',
                 'actionPoints2',
-                'attackerCard',
-                'amountOfCardsToDiscard',
                 'queryEvents',
-                'allOpponentStationCards',
-                'allPlayerStationCards',
                 'playerRetreated',
                 'opponentRetreated',
                 'turnControl',
-                'startingStationCardsToPutDownCount',
                 'gameConfig',
-                'choosingStartingPlayer',
-                'isOwnTurn',
                 'gameOn',
                 'playerPerfectPlan',
                 'playerRuleService',
-                'lastStand'
             ]),
             ...requirementHelpers.mapGetters([
                 'waitingForOtherPlayerToFinishRequirements',
-                'waitingRequirement',
                 'firstRequirement',
-                'firstRequirementIsDiscardCard',
-                'firstRequirementIsDamageStationCard',
                 'firstRequirementIsDrawCard',
                 'firstRequirementIsFindCard',
                 'firstRequirementIsCounterCard',
                 'firstRequirementIsCounterAttack',
                 'cardsLeftToSelect',
-                'selectedCardsCount',
                 'countInFirstRequirement',
                 'requirementCardImageUrl'
             ]),
             ...cardHelpers.mapState([
-                'activeAction',
                 'holdingCard'
             ]),
             ...cardHelpers.mapGetters([
@@ -397,48 +205,19 @@
                 'canDrawCards',
                 'canMill',
                 'canIssueOverwork',
-                'opponentHasControlOfPlayersTurn',
-                'playerHasControlOfOpponentsTurn'
+                'opponentHasControlOfPlayersTurn'
             ]),
-            ...ghostHelpers.mapGetters([
-                'activateEventCardGhostVisible'
-            ]),
-            ...infoModeHelpers.mapState({
-                infoModeVisible: 'visible'
-            }),
             startGameButtonContainerVisible() {
                 return this.readyButtonVisible;
             },
             nextPhaseButtonContainerVisible() {
                 return this.gameOn && !this.holdingCard;
             },
-            guideTextContainerVisible() {
-                if (this.choosingStartingPlayer) return false;
-                if (this.activateEventCardGhostVisible) return false;
-                if (this.infoModeVisible) return false;
-
-                return true;
-            },
             overworkContainerVisible() {
                 return this.canIssueOverwork && !this.holdingCard;
             },
             perfectPlanContainerVisible() {
                 return this.playerPerfectPlan.canIssuePerfectPlan() && !this.holdingCard;
-            },
-            playerIsReady() {
-                return this.readyPlayerIds.includes(this.ownUser.id);
-            },
-            waitingForOtherPlayerToSelectStartingPlayer() {
-                return this.mode === MatchMode.chooseStartingPlayer && !this.isOwnTurn;
-            },
-            selectingStartingStationCards() {
-                return this.mode === MatchMode.selectStationCards;
-            },
-            showActionPoints() {
-                return ['action'].includes(this.phase);
-            },
-            playerActionPointsText() {
-                return `${this.actionPoints2} action ${pluralize('point', this.actionPoints2)} remaining`;
             },
             gameHasEnded() {
                 return this.hasWonGame || this.hasLostGame;
@@ -451,24 +230,6 @@
             },
             PHASES() {
                 return PHASES;
-            },
-            lastStandText() {
-                if (this.playerHasControlOfOpponentsTurn && !!this.lastStand.hasStarted()) {
-                    if (this.lastStand.hasEnded()) {
-                        return 'You were too late';
-                    }
-                    return `${this.lastStandRemainingSeconds}s left to counter defeat`;
-                }
-                return '';
-            },
-            opponentLastStandText() {
-                if (this.lastStand.hasStarted() && this.lastStandInfo.playerId !== this.ownUser.id) {
-                    if (this.lastStand.hasEnded()) {
-                        return 'You have won';
-                    }
-                    return `Your opponent has ${this.lastStandRemainingSeconds}s left to counter defeat`;
-                }
-                return '';
             },
             endTurnButtonVisible() {
                 return !this.nextPhaseWithAction || this.nextPhaseWithAction === PHASES.wait;
@@ -494,51 +255,6 @@
                     && !this.inDiscardPhaseAndMustDiscardCard
                     && this.phase !== PHASES.wait
                     && !this.turnControl.opponentHasControlOfPlayersTurn();
-            },
-            numberOfStationCardsToSelect() {
-                if (!this.attackerCard) return 0;
-                if (this.selectedDefendingStationCards.length === 0) return 0;
-
-                return this.attackerCard.attack - this.selectedDefendingStationCards.length;
-            },
-            requirementGuideText() {
-                if (this.firstRequirementIsDiscardCard) {
-                    const cardsToDiscard = this.countInFirstRequirement;
-                    return `Discard ${cardsToDiscard} ${pluralize('card', cardsToDiscard)}`;
-                }
-                else if (this.firstRequirementIsDamageStationCard && this.cardsLeftToSelect > 0) {
-                    return `Select ${this.cardsLeftToSelect} station ${pluralize('card',
-                        this.cardsLeftToSelect)} to damage`;
-                }
-                else if (this.firstRequirementIsDrawCard) {
-                    return this.drawCardOrMillText;
-                }
-                else {
-                    return '';
-                }
-            },
-            shouldShowSkipDrawCard() {
-                return this.firstRequirementIsDrawCard
-                    && this.firstRequirement.cardCommonId === FatalError.CommonId;
-            },
-            actionGuideText() {
-                if (!this.activeAction) return '';
-                if (this.activeAction.text) {
-                    return this.activeAction.text;
-                }
-                return '';
-            },
-            textOnWaitPhase() {
-                if (this.playerHasControlOfOpponentsTurn) {
-                    return 'Put down any 0-cost card';
-                }
-                return 'Enemy turn';
-            },
-            turnControlButtonText() {
-                if (this.playerHasControlOfOpponentsTurn) {
-                    return 'Release control';
-                }
-                return 'Take control';
             },
             cardStyle() {
                 if (this.activeActionCardImageUrl) {
@@ -573,28 +289,11 @@
                 return `Draw ${count} ${pluralize('card', count)}`;
             }
         },
-        watch: {
-            lastStandInfo: {
-                handler() {
-                    if (this.lastStandInfo === null) return;
-
-                    this.lastStandUpdateIntervalId = setInterval(() => {
-                        this.lastStandRemainingSeconds = this.lastStand.remainingSeconds();
-                        if (this.lastStandRemainingSeconds === 0) clearInterval(this.lastStandUpdateIntervalId);
-                    }, 200);
-                },
-                immediate: true
-            }
-        },
-        destroyed() {
-            if (this.lastStandRemainingSeconds === 0) clearInterval(this.lastStandUpdateIntervalId);
-        },
         methods: {
             ...mapActions([
                 'goToNextPhase',
                 'overwork',
                 'perfectPlan',
-                'toggleControlOfTurn'
             ]),
             ...startGameHelpers.mapActions([
                 'playerReady'
@@ -605,12 +304,12 @@
             nextPhaseClick() {
                 this.goToNextPhase();
             },
+            hideEnlargedCard() {
+                this.enlargedCardVisible = false;
+            },
             showEnlargedCard() { //TODO Should use new expandedCard component instead!
                 this.enlargedCardVisible = true;
             },
-            hideEnlargedCard() {
-                this.enlargedCardVisible = false;
-            }
         },
     };
 
@@ -784,19 +483,5 @@
         .playerDrawPileDescription, .opponentDrawPileDescription {
             display: none;
         }
-    }
-
-    .guideTextCardWrapper {
-        width: $cardWidth * .1;
-        height: $cardHeight * .1;
-        position: relative;
-        top: 4px;
-        margin-right: 15px;
-        flex: 0 0 auto;
-    }
-
-    .guideTextCard {
-        width: 100%;
-        height: 100%;
     }
 </style>
