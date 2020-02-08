544c464d81a90405403b3e4a928ccebf6ea5bc9e
August Alexandersson <augustalexandersson@gmail.com>
2020-02-08 13:54:51 +0100

    Removed bocha watchAll tests file from /server

--- server/test/watchAll.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const bocha = require('bocha');
-
-const srcPath = path.join(__dirname, '..', '..');
-const testPath = __dirname;
-bocha.watch({fileSuffix: '.tests.js', srcPath, testPath});
