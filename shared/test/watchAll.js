7cf6a1dcf3c9214afe54a6d4d1514dd07651ba61
August Alexandersson <augustalexandersson@gmail.com>
2020-02-08 14:04:09 +0100

    Removed Bocha watchAll tests script from /shared

--- shared/test/watchAll.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-const bocha = require('bocha');
-
-const srcPath = path.join(__dirname, '..');
-bocha.watch({ fileSuffix: '.tests.js', srcPath, testPath: __dirname });
