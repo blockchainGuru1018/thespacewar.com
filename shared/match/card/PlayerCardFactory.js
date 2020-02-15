5a52c6383cb2ba210dceb8f34251653d712c6466
August Alexandersson <augustalexandersson@gmail.com>
2020-02-15 13:27:31 +0100

    ...

--- shared/match/card/PlayerCardFactory.js
+++ shared/match/card/PlayerCardFactory.js
@@ -3,11 +3,16 @@ module.exports = function ({
     cardFactory
 }) {
     return {
-        fromId
+        fromId,
+        fromData
     };
 
     function fromId(cardId) {
         const cardData = playerStateService.findCardFromAnySource(cardId);
+        return fromData(cardData);
+    }
+
+    function fromData(cardData) {
         return cardFactory.createCardForPlayer(cardData, playerStateService.getPlayerId());
     }
 };
