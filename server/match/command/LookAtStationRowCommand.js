dd4bc97d2bd38753b576bb841d38bd9383f7d8c4
August Alexandersson <augustalexandersson@gmail.com>
2020-02-15 15:48:35 +0100

    Throw error when incorrectly tries to look at station row

--- server/match/command/LookAtStationRowCommand.js
+++ server/match/command/LookAtStationRowCommand.js
@@ -1,7 +1,12 @@
+const CheatError = require('../../../shared/match/card/CheatError.js');
+
 module.exports = function LookAtStationRowCommand({
     lookAtStationRow
 }) {
     return ({ cardId }) => {
+        if (!lookAtStationRow.cardCanDoIt(cardId)) {
+            throw new CheatError('Cannot look at station row');
+        }
         if (lookAtStationRow.cardCanDoIt(cardId)) {
             lookAtStationRow.doIt();
         }
--- server/test/LookAtStationRowCommand.spec.js
+++ server/test/LookAtStationRowCommand.spec.js
@@ -14,14 +14,36 @@ test('get card and trigger effect', () => {
     expect(lookAtStationRow.doIt).toBeCalled();
 });
 
-test('when can NOT perform action', () => {
+test('when can NOT perform action, should not perform it', () => {
     const lookAtStationRow = {
         cardCanDoIt: () => false,
         doIt: jest.fn()
     };
     const command = Command({ lookAtStationRow });
 
-    command({ cardId: 'C2A' });
+    catchError(() => command({ cardId: 'C2A' }));
 
     expect(lookAtStationRow.doIt).not.toBeCalled();
 });
+
+test('when can NOT perform action, should throw an error!', () => {
+    const lookAtStationRow = {
+        cardCanDoIt: () => false,
+        doIt: jest.fn()
+    };
+    const command = Command({ lookAtStationRow });
+
+    const error = catchError(() => command({ cardId: 'C2A' }));
+
+    expect(error).toBeDefined();
+    expect(error.message).toBe('Cannot look at station row');
+});
+
+function catchError(callback) {
+    try {
+        callback();
+    }
+    catch (error) {
+        return error;
+    }
+}
