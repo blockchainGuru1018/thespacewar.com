56b5f4db7b1163cf6f7b622689ba0781beead2a1
August Alexandersson <augustalexandersson@gmail.com>
2020-02-16 15:11:43 +0100

    Pre-commit hook test

--- server/test/CancelRequirement.integration.spec.js
+++ server/test/CancelRequirement.integration.spec.js
@@ -18,6 +18,7 @@ test('when has 1 cancelable requirement and cancels requirement', () => {
 
     match.cancelRequirement('P1A');
 
+    expect(true).toBe(false);
     firstPlayerAsserter.refuteHasRequirement({ type: 'findCard' });
 });
 
