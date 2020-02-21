d395c2d38e1aaf94489f4ccfcc47b93f0a2ba2bd
August Alexandersson <augustalexandersson@gmail.com>
2020-02-21 20:47:39 +0100

    Refactor

--- client/tests/bot/ActionPhase_integration.test.js
+++ client/tests/bot/ActionPhase_integration.test.js
@@ -7,59 +7,57 @@ const { PHASES } = require('../../../shared/phases.js');
 const { setupFromState, BotId, PlayerId } = require('./botTestHelpers.js');
 const { unflippedStationCard } = require('../../testUtils/factories.js');
 
-describe('Being in the action phase', () => {
-    it('playing a card', async () => {
-        const { matchController } = await setupFromState({
-            turn: 1,
-            phase: 'action',
-            stationCards: [
-                unflippedStationCard('S1A', 'draw')
-            ],
-            cardsOnHand: [
-                createCard({ id: 'C1A', cost: 0, type: 'spaceShip' }),
-            ]
-        });
-
-        expect(matchController.emit).toBeCalledWith('putDownCard', { location: 'zone', cardId: 'C1A' });
+test('playing a card', async () => {
+    const { matchController } = await setupFromState({
+        turn: 1,
+        phase: 'action',
+        stationCards: [
+            unflippedStationCard('S1A', 'draw')
+        ],
+        cardsOnHand: [
+            createCard({ id: 'C1A', cost: 0, type: 'spaceShip' }),
+        ]
     });
 
-    it('cannot play a card, should NOT try to play a card', async () => {
-        const { matchController } = await setupFromState({
-            turn: 1,
-            phase: 'action',
-            stationCards: [
-                unflippedStationCard('S1A', 'draw')
-            ],
-            cardsOnHand: [
-                createCard({ id: 'C1A', cost: 1 }),
-            ]
-        });
+    expect(matchController.emit).toBeCalledWith('putDownCard', { location: 'zone', cardId: 'C1A' });
+});
 
-        expect(matchController.emit).not.toBeCalledWith('putDownCard');
+test('cannot play a card, should NOT try to play a card', async () => {
+    const { matchController } = await setupFromState({
+        turn: 1,
+        phase: 'action',
+        stationCards: [
+            unflippedStationCard('S1A', 'draw')
+        ],
+        cardsOnHand: [
+            createCard({ id: 'C1A', cost: 1 }),
+        ]
     });
 
-    it('when has NO card to play, should proceed to next phase', async () => {
-        const { matchController } = await setupFromState({
-            turn: 1,
-            phase: 'action',
-            stationCards: [
-                unflippedStationCard('S1A', 'draw')
-            ],
-            cardsOnHand: []
-        });
+    expect(matchController.emit).not.toBeCalledWith('putDownCard');
+});
 
-        expect(matchController.emit).toBeCalledWith('nextPhase', { currentPhase: PHASES.action });
+test('when has NO card to play, should proceed to next phase', async () => {
+    const { matchController } = await setupFromState({
+        turn: 1,
+        phase: 'action',
+        stationCards: [
+            unflippedStationCard('S1A', 'draw')
+        ],
+        cardsOnHand: []
     });
 
-    it('when has card too expensive to play, should place as station card', async () => {
-        const { matchController } = await setupFromState({
-            turn: 1,
-            phase: 'action',
-            cardsOnHand: [
-                createCard({ id: 'C1A', cost: 1 })
-            ]
-        });
+    expect(matchController.emit).toBeCalledWith('nextPhase', { currentPhase: PHASES.action });
+});
 
-        expect(matchController.emit).toBeCalledWith('putDownCard', { cardId: 'C1A', location: expect.any(String) });
+test('when has card too expensive to play, should place as station card', async () => {
+    const { matchController } = await setupFromState({
+        turn: 1,
+        phase: 'action',
+        cardsOnHand: [
+            createCard({ id: 'C1A', cost: 1 })
+        ]
     });
+
+    expect(matchController.emit).toBeCalledWith('putDownCard', { cardId: 'C1A', location: expect.any(String) });
 });
