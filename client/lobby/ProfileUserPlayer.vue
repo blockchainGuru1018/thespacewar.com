53a6df8d3aa559aa3ac16640369fade037486e56
unknown <keiro3@gmail.com>
2020-05-13 17:09:15 -0400

    Imporve lobby section

--- client/lobby/ProfileUserPlayer.vue
+++ client/lobby/ProfileUserPlayer.vue
@@ -101,7 +101,14 @@
         padding: 6px 10px;
         text-align: left;
         opacity: 1;
-        // background-color: #0000007d;
+        background: url(https://images.thespacewar.com/corner-top-name-player.png) no-repeat top left,
+                    url(https://images.thespacewar.com/corner-bottom-name-player.png) no-repeat bottom left,
+                    url(https://images.thespacewar.com/corner-top-right-name-player.png) no-repeat top right,
+                    url(https://images.thespacewar.com/corner-bottom-right-name-player.png) no-repeat bottom right,
+                    url(https://images.thespacewar.com/name-player-bg.jpg) no-repeat center right;
+        background-size: auto, auto, auto, auto, cover;
+        width: 100%;
+        max-width: 300px;
         margin-bottom: 15px;
         .username {
             text-transform: capitalize;
