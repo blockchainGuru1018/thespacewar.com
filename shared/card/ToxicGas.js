a1b55fcbb73b6e8c623f93292e99ea6e434c0082
Rene Huanco <angel_genezzi_roy@hotmail.com>
2020-05-24 21:56:46 -0400

    Work in toxic gas card...

--- shared/card/ToxicGas.js
+++ shared/card/ToxicGas.js
@@ -4,9 +4,18 @@ const CanMoveFirstTurn = require('./mixins/CanMoveFirstTurn.js');
 module.exports = class ToxicGas extends CanMoveFirstTurn(BaseCard) {
     constructor(deps) {
         super({...deps});
+        simulateAttack()
     }
 
     static get CommonId() {
         return '79';
+    }
+
+    damageWhenAttackedMultiplier() {
+        return 2;
+    }
+
+    simulateAttack() {
+        return this.damageWhenAttackedMultiplier() - 1;
     }
 };
