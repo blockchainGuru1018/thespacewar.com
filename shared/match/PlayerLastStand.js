fdfcb4a5413d6f2590281ea90a1ebaf9a9bbd8e9
David Barrera <davp@trabajoendigital.com>
2020-06-22 19:29:27 -0500

    Fix - The bot should not receive last stand
    Also August help me a lot to refractor the PlayerLastStand and MatchComService files

--- shared/match/PlayerLastStand.js
+++ shared/match/PlayerLastStand.js
@@ -2,10 +2,13 @@ module.exports = function ({
     playerId,
     matchService,
     playerTurnControl,
-    opponentTurnControl
+    opponentTurnControl,
+    playerStateService,
+    queryPlayerRequirements,
 }) {
     return {
-        start
+        start,
+        canStart
     };
 
     function start() {
@@ -19,5 +22,29 @@ module.exports = function ({
         else if (playerTurnControl.opponentHasControlOfOwnTurn()) {
             playerTurnControl.takeControlOfOpponentsTurn();
         }
+    }
+
+    function canStart() {
+      console.log("playerStateService.isBot(): ", playerStateService.isBot());
+      if (playerStateService.isBot()) return false;
+      const allStationCardsAreDamaged =
+        playerStateService.getUnflippedStationCardsCount() === 0;
+      return allStationCardsAreDamaged && canAvoidStationCardAttack();
+    }
+  
+    function canAvoidStationCardAttack() {
+      const cardsCanBeUsedToCounter = playerStateService.getMatchingPlayableBehaviourCards(
+        (card) => {
+          return card.canCounterCardsBeingPlayed || card.canCounterAttacks;
+        }
+      );
+      const isCurrentlyUsingCounter =
+        queryPlayerRequirements.getFirstMatchingRequirement({
+          type: "counterCard",
+        }) ||
+        queryPlayerRequirements.getFirstMatchingRequirement({
+          type: "counterAttack",
+        });
+      return cardsCanBeUsedToCounter.length > 0 || isCurrentlyUsingCounter;
     }
 };
---
+++ shared/test/PlayerLastStand.spec.js
@@ -0,0 +1,32 @@
+const PlayerLastStand = require("../match/PlayerLastStand.js");
+function createPLayerLastStand({ isBot = () => false } = {}) {
+  return PlayerLastStand({
+    playerId: "",
+    matchService: {},
+    playerTurnControl: {},
+    opponentTurnControl: {},
+    playerStateService: {
+      getUnflippedStationCardsCount: () => 0,
+      getMatchingPlayableBehaviourCards: () => [{}],
+      isBot,
+    },
+
+    queryPlayerRequirements: {
+      getFirstMatchingRequirement: () => null,
+    },
+  });
+}
+
+describe("Can enter last stand", () => {
+  test("When have a card that can be used to counter and all station cards are flipped", () => {
+    const playerLastStand = createPLayerLastStand();
+    expect(playerLastStand.canStart()).toBe(true);
+  });
+});
+
+describe("Can NOT enter last stand", () => {
+  test("When player is a Bot", () => {
+    const playerLastStand = createPLayerLastStand({ isBot: () => true });
+    expect(playerLastStand.canStart()).toBe(false);
+  });
+});
