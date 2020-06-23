ecce9631e295debb13d4cb4a3a627f2069d3008f
August Alexandersson <augustalexandersson@gmail.com>
2020-06-23 16:01:17 +0200

    Updated the way the server install dependencies

--- shared/serverStarter.js
+++ shared/serverStarter.js
@@ -11,9 +11,9 @@ module.exports = {
 };
 
 function installNpmPackages() {
-    const scriptPath = path.join(__dirname, '..', 'scripts');
+    const scriptPath = path.join(__dirname);
     console.info(' (1/2) - Installing dependencies');
-    child_process.execSync(`cd ${scriptPath} && bash ./install`);
+    child_process.execSync(`cd ${scriptPath} && npm install-all`);
 }
 
 function startServer() {
