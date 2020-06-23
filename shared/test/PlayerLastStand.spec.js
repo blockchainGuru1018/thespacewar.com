61eacae666b265273efee08dc05272d2a38183a7
David Barrera <davp@trabajoendigital.com>
2020-06-23 18:38:15 -0500

    Moved createPlayerLastStand below all tests.

--- shared/test/PlayerLastStand.spec.js
+++ shared/test/PlayerLastStand.spec.js
@@ -1,4 +1,20 @@
 const PlayerLastStand = require("../match/PlayerLastStand.js");
+
+describe("Can enter last stand", () => {
+  test("When have a card that can be used to counter and all station cards are flipped", () => {
+    const playerLastStand = createPLayerLastStand();
+    expect(playerLastStand.canStart()).toBe(true);
+  });
+});
+
+describe("Can NOT enter last stand", () => {
+  test("When player is a Bot", () => {
+    const playerLastStand = createPLayerLastStand({ isBot: () => true });
+    expect(playerLastStand.canStart()).toBe(false);
+  });
+});
+
+
 function createPLayerLastStand({ isBot = () => false } = {}) {
   return PlayerLastStand({
     playerId: "",
@@ -16,17 +32,3 @@ function createPLayerLastStand({ isBot = () => false } = {}) {
     },
   });
 }
-
-describe("Can enter last stand", () => {
-  test("When have a card that can be used to counter and all station cards are flipped", () => {
-    const playerLastStand = createPLayerLastStand();
-    expect(playerLastStand.canStart()).toBe(true);
-  });
-});
-
-describe("Can NOT enter last stand", () => {
-  test("When player is a Bot", () => {
-    const playerLastStand = createPLayerLastStand({ isBot: () => true });
-    expect(playerLastStand.canStart()).toBe(false);
-  });
-});
