167c3a9c04bc31e63de421a892081bc0b2a6cc28
David Barrera <davp@trabajoendigital.com>
2020-06-29 13:23:09 -0500

    ...

--- shared/test/DestroyDuration.spec.js
+++ shared/test/DestroyDuration.spec.js
@@ -12,9 +12,15 @@ describe('Destroy Duration', () => {
         };
         expect(card.canTargetCardForSacrifice(otherCard)).toBe(true);
     });
-    // can NOT target space ship for sacrifice
-    // should NOT be able to be sacrificed at the same turn as played
-    // should be able to be sacrificed the turn after being played
-    // should NOT be able to be sacrificed if has already attacked this turn
-    // should ONLY be able to be sacrificed in the attack phase
+    it('can NOT target space ship for sacrifice', ()=>{
+        const card = new DestroyDuration({
+            playerId: 'P1A'
+        });
+        const spaceshipCard = {
+            canBeTargeted: () => true,
+            type: 'spaceShip',
+            playerId: 'P2A'
+        };
+        expect(card.canTargetCardForSacrifice(spaceshipCard)).toBe(false);
+    })
 });
