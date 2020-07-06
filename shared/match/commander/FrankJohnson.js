62bb3156f0423f50d9ac172cf27b2f0c980835b8
August Alexandersson <augustalexandersson@gmail.com>
2020-07-06 18:17:24 +0200

    ...

--- shared/match/commander/FrankJohnson.js
+++ shared/match/commander/FrankJohnson.js
@@ -1,4 +1,5 @@
 const GlobalConfig = require('../../../config.json');
+
 module.exports = function () {
     return {
         maxStationCards: () => GlobalConfig.COMMANDER_FRANK_JOHNSON_MAX_STATION_CARDS
