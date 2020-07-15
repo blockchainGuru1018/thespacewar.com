f2c2289393aaf65cd0283fa3ddb71d839969accb
Jim Westergren <jim@jimwestergren.com>
2020-07-15 23:39:53 +0200

    Updating text of AttackPhase.vue tutorial

--- client/match/infoMode/tutorial/steps/AttackPhase.vue
+++ client/match/infoMode/tutorial/steps/AttackPhase.vue
@@ -2,16 +2,15 @@
   <div class="infoMode-step">
     <h2>Attack phase</h2>
     <p>
-      You can now move and attack with your cards.
+      In this phase you can move and attack with your cards in play.
     </p>
     <p>
-      Cards can not move at the same turn as they are played. Later when you do
-      move them, you cannot attack your opponents station card on the same turn
-      they moved there.
+      Cards cannot move the same turn as they are played but they can attack other cards in the same zone.
+      Your spaceships cannot attack the enemy station cards the first turn they moved to the enemy zone. 
     </p>
     <p>
       Cards may have special attacks, or certain properties described on the
-      card itself. You can long press on any card to examine it.
+      card itself. You can right click or long press on any card to examine it.
     </p>
   </div>
 </template>
