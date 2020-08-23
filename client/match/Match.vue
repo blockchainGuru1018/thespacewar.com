977c0c3ff8d7b972affce4185d2a52677ce9d270
David Barrera <davo.barrera@gmail.com>
2020-08-22 23:38:40 -0500

    Fix - Texts and buttons are on top of cards

--- client/match/Match.vue
+++ client/match/Match.vue
@@ -152,23 +152,23 @@
 
             <div class="field-commandersAndDrawPile">
               <OpponentCommanderCards />
-              <div class="field-drawPile">
+              <div class="field-drawPile opponentDrawPile">
                 <portal-target name="opponentDrawPile" />
                 <div
                   v-if="!opponentDeckIsEmpty"
                   class="card card-faceDown pile-3d"
                   :style="
                     'transform: translateZ(calc(1px * ' +
-                      opponentCardPileHeight +
-                      '))'
+                    opponentCardPileHeight +
+                    '))'
                   "
                 >
                   <div
                     class="actionOverlays"
                     :style="
                       'transform: translateZ(calc(1px * ' +
-                      opponentCardPileHeight +
-                        '))'
+                        opponentCardPileHeight +
+                      '))'
                     "
                   >
                     <div
@@ -200,8 +200,8 @@
                   class="drawPile-cardCount drawPile-cardCountText"
                   :style="
                     'transform: translateZ(calc(1px * ' +
-                    opponentCardPileHeight +
-                    '))'
+                      opponentCardPileHeight +
+                      '))'
                   "
                 >
                   {{ opponentCardsInDeckCount }}
@@ -224,8 +224,8 @@
                   class="card card-faceDown pile-3d"
                   :style="
                     'transform: translateZ(calc(1px * ' +
-                    playerCardPileHeight +
-                    '))'
+                      playerCardPileHeight +
+                      '))'
                   "
                 >
                   <div class="actionOverlays">
@@ -253,8 +253,8 @@
                   class="drawPile-cardCount drawPile-cardCountText"
                   :style="
                     'transform: translateZ(calc(1px * ' +
-                    playerCardPileHeight +
-                    '))'
+                      playerCardPileHeight +
+                      '))'
                   "
                 >
                   {{ playerCardsInDeckCount }}
@@ -1007,6 +1007,58 @@ module.exports = {
         ZPosition = Math.max(-50, Math.min(10, ZPosition - 1));
         root.style.setProperty("--z-position", ZPosition + "px");
         setTimeout(this.adjustZoom, 0);
+      } else {
+        this.adjustCenteredGUI();
+      }
+    },
+    centeredGUICollideWithPlayerStationCards() {
+      const rect1 = (
+        document.getElementsByClassName("field-dividerContent")[0] || {}
+      ).getBoundingClientRect();
+      const rect2 = (
+        document.getElementsByClassName("playerStationCards")[0] || {}
+      ).getBoundingClientRect();
+      return !(
+        rect1.right < rect2.left ||
+        rect1.left > rect2.right ||
+        rect1.bottom < rect2.top ||
+        rect1.top > rect2.bottom
+      );
+    },
+    spaceBetweenOpponentDrawpileAndPlayerStationCards() {
+      const rect1 = (
+        document.getElementsByClassName("playerStationCards")[0] || {}
+      ).getBoundingClientRect();
+      const rect2 = (
+        document.getElementsByClassName("opponentDrawPile")[0] || {}
+      ).getBoundingClientRect();
+      return rect1.top - rect2.bottom;
+    },
+    adjustCenteredGUI() {
+      let spaceBetweenOpponentDrawpileAndPlayerStationCards = this.spaceBetweenOpponentDrawpileAndPlayerStationCards();
+      if (spaceBetweenOpponentDrawpileAndPlayerStationCards <= 66) {
+        const root = document.documentElement;
+        let spaceBetween = parseInt(
+          getComputedStyle(document.documentElement)
+            .getPropertyValue("--space-between-GUI-n-draw-pile")
+            .replace("px", "")
+        );
+        spaceBetween = Math.min(50, spaceBetween + 1);
+        root.style.setProperty(
+          "--space-between-GUI-n-draw-pile",
+          spaceBetween + "px"
+        );
+        setTimeout(this.adjustCenteredGUI, 0);
+      } else if (this.centeredGUICollideWithPlayerStationCards()) {
+        const root = document.documentElement;
+        let GUIPosition = parseInt(
+          getComputedStyle(document.documentElement)
+            .getPropertyValue("--gui-height-adjust")
+            .replace("%", "")
+        );
+        GUIPosition = Math.max(30, Math.min(50, GUIPosition - 1));
+        root.style.setProperty("--gui-height-adjust", GUIPosition + "%");
+        setTimeout(this.adjustCenteredGUI, 0);
       }
     },
     validateBG() {
