50d0c60bb0e79e0c4c22d39fd6689de3aa4df1c4
manuel <manuelclimb24@gmail.com>
2020-11-20 21:12:33 -0300

    paralizer player removed

--- client/ai/cardRules/CardRulesFactory.js
+++ client/ai/cardRules/CardRulesFactory.js
@@ -1,7 +1,6 @@
 const ShouldPlayGoodKarma = require("./ShouldPlayGoodKarma.js");
 const ShouldPlayDroneLeader = require("./ShouldPlayDroneLeader.js");
 const ShouldPlayRepairShip = require("./ShouldPlayRepairShip.js");
-const ShouldPlayParalizer = require("./ShouldPlayParalizer.js");
 const ShouldPlayFusionShip = require("./ShouldPlayFusionShip");
 const ShouldPlayReviveProcedure = require("./ShouldPlayReviveProcedure");
 const ShouldPlaySacrifice = require("./ShouldPlaySacrifice");
@@ -16,7 +15,6 @@ module.exports = function ({ BotId, opponentUserId, playerServiceFactory }) {
       shouldPlayGoodKarma(),
       shouldPlayDroneLeader(),
       shouldPlayRepairShip(),
-      shouldPlayParalizer(),
       shouldPlayFusionShip(),
       shouldPlayReviveProcedure(),
       shouldPlaySacrifice(),
@@ -47,13 +45,7 @@ module.exports = function ({ BotId, opponentUserId, playerServiceFactory }) {
       playerStateService: playerServiceFactory.playerStateService(BotId),
     });
   }
-  function shouldPlayParalizer() {
-    return ShouldPlayParalizer({
-      opponentStateService: playerServiceFactory.playerStateService(
-        opponentUserId
-      ),
-    });
-  }
+
   function shouldPlayFusionShip() {
     return ShouldPlayFusionShip({
       playerStateService: playerServiceFactory.playerStateService(BotId),
