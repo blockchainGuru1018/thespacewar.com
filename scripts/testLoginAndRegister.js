2dc3fbaca3e5f2f17704f30f0d5fc2356079ae9b
Jim Westergren <jim@jimwestergren.com>
2021-01-22 19:31:41 +0100

    Changing login URL

--- scripts/testLoginAndRegister.js
+++ scripts/testLoginAndRegister.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function testLogin() {
   console.log("Login test:");
   try {
-    const response = await axios.post("https://thespacewar.com/login");
+    const response = await axios.post("https://thespacewar.com/account/");
     console.log("RESPONSE", response);
 
     const data = response.data;
@@ -17,7 +17,7 @@ async function testLogin() {
 async function testRegister() {
   console.log("Register test:");
   try {
-    const response = await axios.post("https://thespacewar.com/login");
+    const response = await axios.post("https://thespacewar.com/account/");
     console.log("RESPONSE", response);
 
     const data = response.data;
