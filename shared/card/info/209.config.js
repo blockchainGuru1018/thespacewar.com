2b90febc0d51cab5f47b9b4d5a92cead697398b1
KangDo96 <58952144+KangDo96@users.noreply.github.com>
2021-06-07 01:30:49 +0800

    Create 209.config.js

---
+++ shared/card/info/209.config.js
@@ -0,0 +1,41 @@
+const CommonId = "209";
+const DrawCardsCount = 1;
+
+module.exports = {
+  CommonId,
+  choiceToRequirementSpec: {
+    draw: {
+      forOpponent: [],
+      forPlayer: [
+        {
+          type: "drawCard",
+          count: DrawCardsCount,
+          cardCommonId: CommonId,
+        },
+      ],
+    },
+    counter: {
+      forOpponent: [],
+      forPlayer: [
+        {
+          type: "counterCard",
+          count: 1,
+          sources: ["opponentAny"],
+          filter: {
+            canBeCountered: true,
+          },
+        },
+      ],
+    },
+  },
+  choicesWhenPutDownInHomeZone: [
+    {
+      name: "counter",
+      text: "Counter a card costing 0 or 1. You may draw a card and discard a card.",
+    },
+    {
+      name: "draw",
+      text: `Draw ${DrawCardsCount} cards`,
+    },
+  ],
+};
